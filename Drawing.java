4adcfaad94c357c5971c7e66c0b9bcbb7acbcfed
DaoTimothy <timothydao273@gmail.com>
2021-06-29 14:38:25 -0400

    Update Drawing.java

--- Drawing.java
+++ Drawing.java
@@ -1,13 +1,19 @@
 import java.awt.Canvas;
 import java.awt.Graphics;
+import java.awt.Color;
 import javax.swing.JFrame;
 public class Drawing extends Canvas{
     public static void main (String[] args) {
-        JFrame frame = new JFrame("Screen");
+        JFrame frame = new JFrame("League of Invaders 2");
         Canvas canvas = new Drawing();
-        canvas.setSize(400, 400);
+        canvas.setSize(500, 500);
         frame.add(canvas);
         frame.pack();
         frame.setVisible(true);
+    }
+
+    public void paint (Graphics g) {
+        g.setColor(Color.red);
+        g.fillOval(150, 150, 200, 200);
     }
 }

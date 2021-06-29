8a1343beacde1b6d59e392a0efd984ccfa5aa27e
DaoTimothy <timothydao273@gmail.com>
2021-06-28 21:25:50 -0400

    Update Game.java

--- Game.java
+++ Game.java
@@ -1,5 +1,11 @@
 class Game {
     public static void main (String[] args) {
         System.out.println ("Main Code");
+<<<<<<< Updated upstream
+=======
+
+        System.out.println ("Function A");
+        System.out.println ("Commit to main branch");
+>>>>>>> Stashed changes
     }
 }
